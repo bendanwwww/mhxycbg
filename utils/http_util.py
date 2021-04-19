e0945c7b7fd637fb201441082d0085ef9d64fd96
bendanwwww <461806307@qq.com>
2021-04-19 18:54:24 +0800

    优化代理

--- utils/http_util.py
+++ utils/http_util.py
@@ -11,10 +11,14 @@ class http_util(object):
         return requests.get(url, headers=header, params=params, timeout = 10)
 
     def set_proxy(self):
-        # url = 'http://http.tiqu.letecs.com/getip3?num=1&type=1&pro=&city=0&yys=0&port=1&pack=141532&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=&gm=4'
-        # proxy = requests.get(url).text
-        os.environ["http_proxy"] = "http://42.56.35.211:4240"
-        os.environ["https_proxy"] = "https://42.56.35.211:4240"
+        if "http_proxy" in os.environ:
+            del os.environ["http_proxy"]
+        if "https_proxy" in os.environ:
+            del os.environ["https_proxy"]
+        url = 'http://http.tiqu.letecs.com/getip3?num=1&type=1&pro=&city=0&yys=0&port=11&pack=141532&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=2&regions=&gm=4'
+        proxy_url = self.http_get(None, None, url).text
+        os.environ["http_proxy"] = str("http://" + proxy_url)
+        os.environ["https_proxy"] = str("https://" + proxy_url)
 
     def get_cbg_info_proxy(self, params, retry):
         url = "https://xyq.cbg.163.com/cgi-bin/recommend.py"
